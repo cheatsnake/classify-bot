1b9df56cb72198e5f964216877ffdb712ef49a69
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-01-24 17:45:55 +0300

    Add help messages

---
+++ bot/messages/help.msg.js
@@ -0,0 +1,11 @@
+const help = `
+Here are all the commands of the bot:
+
+/encode - Encrypt a text message using a given key.
+/decode - Decrypt an encrypted message using a key.
+/keygen - Generate a random key of a given length.
+
+If you have a problem, write to @Push0k
+`;
+
+module.exports = { help };
